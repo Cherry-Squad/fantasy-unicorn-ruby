4877c717c9251c872888302a446d2a2236b82393
Konstantin <info@a6raywa1cher.com>
2021-11-02 13:55:23 +0300

    Add comment on Achievement record

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# This class represents an Achievement record.
+# The Achievement model represents an achievement obtained by a User.
+#
+# User < Achievement: many-to-one, unique achievement_identifier per user_id
 class Achievement < ApplicationRecord
   belongs_to :user
 
