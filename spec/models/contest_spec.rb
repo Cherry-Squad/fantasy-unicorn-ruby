e7fe7b1e4e41438ed599b53290160f75f0ba1a13
Konstantin <info@a6raywa1cher.com>
2021-11-25 20:51:19 +0300

    Refactor contest_spec.rb

--- spec/models/contest_spec.rb
+++ spec/models/contest_spec.rb
@@ -3,120 +3,130 @@
 require 'rails_helper'
 
 RSpec.describe Contest, type: :model do
-  it 'is valid with bare minimum attributes' do
-    expect(create(:contest)).to be_valid
+  subject { create(:contest) }
+
+  it 'is valid with valid arguments' do
+    is_expected.to be_valid
   end
 
-  context 'with preconfigured stocks' do
-    before do
-      @contest = create :contest, :with_stocks
-      @stocks = @contest.stocks
-    end
+  context 'created with preconfigured stocks' do
+    subject { create(:contest, :with_stocks) }
+    let(:stocks) { subject.stocks }
 
     it 'is valid' do
-      expect(@contest).to be_valid
+      is_expected.to be_valid
     end
 
     it "can't have the same stock twice" do
-      expect { @contest.stocks << @stocks.sample }.to raise_error(ActiveRecord::RecordNotUnique)
+      expect { subject.stocks << stocks.sample }.to raise_error(ActiveRecord::RecordNotUnique)
     end
 
     it "doesn't destroy stock on destroy" do
-      expect { @contest.destroy }.not_to change(Stock, :count)
+      expect { subject.destroy }.not_to change(Stock, :count).from(stocks.size)
     end
 
     it "doesn't destroy on destroy stock" do
-      expect { @stocks.sample.destroy }.not_to change(Contest, :count)
+      expect { stocks.sample.destroy }.not_to change { Contest.exists?(id: subject.id) }.from(true)
     end
   end
 
-  context 'with registered users' do
-    before do
-      @contest = create(:contest, :with_participants)
-      @applications = @contest.contest_applications
-    end
+  context 'created with registered users' do
+    subject { create(:contest, :with_participants) }
+    let(:applications) { subject.contest_applications }
 
     it 'is valid' do
-      expect(@contest).to be_valid
+      is_expected.to be_valid
     end
 
     it "isn't valid if status changed to finished" do
-      expect { @contest.status = Contest.statuses[:finished] }.to change(@contest, :valid?).to(false)
+      expect { subject.status = Contest.statuses[:finished] }.to change(subject, :valid?).to(false)
     end
 
-    it 'can be transformed to finished' do
-      expect do
-        Contest.transaction do
-          @contest.status = Contest.statuses[:finished]
-          i = 1
-          @applications.each do |application|
-            application.final_position = i
-            i += 1
-            application.coins_delta = 1
-            application.fantasy_points_delta = 1
-            application.save!
-          end
-          @contest.save!
+    context 'transformable to finished' do
+      before { transform }
+
+      def transform
+        subject.status = Contest.statuses[:finished]
+        i = 1
+        applications.each do |application|
+          application.final_position = i
+          i += 1
+          application.coins_delta = 1
+          application.fantasy_points_delta = 1
+          application.save!
         end
-      end.to_not raise_error
-      expect(@contest).to be_valid
-      @applications.each { |a| expect(a).to be_valid }
+        subject.save!
+      end
+
+      it 'and contest would be still valid' do
+        is_expected.to be_valid
+      end
+
+      it 'and all stocks are valid' do
+        applications.each { |a| expect(a).to be_valid }
+      end
     end
   end
 
-  it 'is valid with registered users and preconfigured stocks' do
-    expect(create(:contest, :with_participants, :with_stocks)).to be_valid
+  context 'created with registered users and preconfigured stocks' do
+    subject { create(:contest, :with_participants, :with_stocks) }
+
+    it 'is valid' do
+      is_expected.to be_valid
+    end
   end
 
   it "isn't valid without reg ending timestamp" do
-    expect { create :contest, reg_ending_at: nil }.to raise_error(ActiveRecord::RecordInvalid)
+    subject.reg_ending_at = nil
+    is_expected.to_not be_valid
   end
 
   it "isn't valid without summarizing timestamp" do
-    expect { create :contest, summarizing_at: nil }.to raise_error(ActiveRecord::RecordInvalid)
+    subject.summarizing_at = nil
+    is_expected.to_not be_valid
   end
 
   it "isn't valid with negative coins entry fee" do
-    expect { create :contest, coins_entry_fee: -Faker::Number.number.abs }.to raise_error(ActiveRecord::RecordInvalid)
+    subject.coins_entry_fee = -Faker::Number.number.abs
+    is_expected.to_not be_valid
   end
 
   it "isn't valid with negative max fantasy points threshold" do
-    expect do
-      create :contest, max_fantasy_points_threshold: -Faker::Number.number.abs
-    end.to raise_error(ActiveRecord::RecordInvalid)
+    subject.max_fantasy_points_threshold = -Faker::Number.number.abs
+    is_expected.to_not be_valid
   end
 
   it "isn't valid without direction strategy" do
-    expect { create :contest, direction_strategy: nil }.to raise_error(ActiveRecord::RecordInvalid)
+    subject.direction_strategy = nil
+    is_expected.to_not be_valid
   end
 
   it "isn't valid without status" do
-    expect { create :contest, status: nil }.to raise_error(ActiveRecord::RecordInvalid)
+    subject.status = nil
+    is_expected.to_not be_valid
   end
 
   it "can't have fixed direction up if direction strategy isn't 'fixed'" do
-    expect do
-      create :contest, direction_strategy: Contest.direction_strategies[:free], fixed_direction_up: false
-    end.to raise_error(ActiveRecord::RecordInvalid)
+    subject.direction_strategy = Contest.direction_strategies[:free]
+    subject.fixed_direction_up = false
+    is_expected.to_not be_valid
   end
 
   it "can have fixed direction up if direction strategy is 'fixed'" do
-    direction_strategy = Contest.direction_strategies[:fixed]
-    expect(create(:contest, direction_strategy: direction_strategy, fixed_direction_up: false)).to be_valid
+    subject.direction_strategy = Contest.direction_strategies[:fixed]
+    subject.fixed_direction_up = false
+    is_expected.to be_valid
   end
 
   context 'finished and with users' do
-    before do
-      @contest = create :contest, :with_participants, :finished
-      @applications = @contest.contest_applications
-    end
+    subject { create(:contest, :with_participants, :finished) }
+    let(:applications) { subject.contest_applications }
 
     it 'is valid' do
-      expect(@contest).to be_valid
+      is_expected.to be_valid
     end
-
     it "can't return status back" do
-      expect { @contest.status = Contest.statuses[:created] }.to change(@contest, :valid?).to(false)
+      expect { subject.status = Contest.statuses[:created] }.to change(subject, :valid?).to(false)
     end
   end
 end
