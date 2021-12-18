b637c987bbd46785c5e8db8ad052e1754089490f
Vladimir Konyukhov <konyukhov.vladimir.a@gmail.com>
2021-12-18 16:04:59 +0300

    Rubocop corrections

--- app/controllers/api/v1/auth/registration_controller.rb
+++ app/controllers/api/v1/auth/registration_controller.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
-module Api::V1
-  # Adds username as required parameter for registration and update
-  class Auth::RegistrationController < DeviseTokenAuth::RegistrationsController
-    before_action :configure_permitted_parameters
+module Api
+  module V1
+    module Auth
+      # Adds username as required parameter for registration and update
+      class RegistrationController < DeviseTokenAuth::RegistrationsController
+        before_action :configure_permitted_parameters
 
-    protected
+        protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[username])
+        def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
+          devise_parameter_sanitizer.permit(:account_update, keys: %i[username])
+        end
+      end
     end
   end
 end
