9a1c2d401006613f5f6113bef0bf6a73aa945a4b
Габриэль Осипов <gabriel.osipov@saltpepper.ru>
2022-01-06 17:49:01 +0300

    delete update method

--- app/controllers/api/v1/contests/contest_controller.rb
+++ app/controllers/api/v1/contests/contest_controller.rb
@@ -5,7 +5,7 @@ module Api
     module Contests
       # ContestController class
       class ContestController < ApplicationController
-        # before_action :authenticate_api_v1_user!
+        before_action :authenticate_api_v1_user!
 
         def create
           contest = Contest.new(
@@ -32,21 +32,6 @@ module Api
         def index
           contests = all_contests
           render json: contests, status: 200
-        end
-
-        def update
-          contest = contest_by_id
-          if contest
-            if contest.update(update_params)
-              render json: contest, status: 201
-            else
-              render json: { error: "an error occurred #{contest.errors.full_messages}" }, status: 400
-            end
-          else
-            render json: { status: 'Not Found 404' }, status: 404
-          end
-        rescue StandardError => e
-          render json: { error: "an error occurred #{e.message}" }, status: 400
         end
 
         def delete
@@ -85,47 +70,12 @@ module Api
                                           ])
         end
 
-        def update_params
-          params.require(:contest).permit(%i[
-                                            id
-                                            coins_entry_fee
-                                            direction_strategy
-                                            fixed_direction_up
-                                            max_fantasy_points_threshold
-                                            reg_ending_at
-                                            status
-                                            summarizing_at
-                                            use_briefcase_only
-                                            use_disabled_multipliers
-                                            use_inverted_stock_prices
-                                          ])
-        end
-
         def contest_by_id
           @contest = Contest.find_by(id: params[:id])
         end
 
         def all_contests
-          filter_params = params.permit(:status, :direction_strategy, :fixed_direction_up, :use_briefcase_only,
-                                        :use_disabled_multipliers, :use_inverted_stock_prices)
-                                .delete_if { |_key, value| value.blank? }.transform_values! { |v| v.split(',') }
-          int_filter_params = params.permit(:coins_entry_fee_min_edge, :coins_entry_fee_max_edge,
-                                            :max_fantasy_points_threshold).delete_if { |_key, value| value.blank? }
-          max_coins_entry_fee_edge = if int_filter_params[:coins_entry_fee_max_edge].presence
-                                       int_filter_params[:coins_entry_fee_max_edge].to_i
-                                     else
-                                       Float::INFINITY
-                                     end
-          fantasy_points_threshold_edge = if int_filter_params[:max_fantasy_points_threshold].presence
-                                            int_filter_params[:max_fantasy_points_threshold].to_i
-                                          else
-                                            Float::INFINITY
-                                          end
-          filter_by_int = {
-            coins_entry_fee: (int_filter_params[:coins_entry_fee_min_edge].to_i..max_coins_entry_fee_edge),
-            max_fantasy_points_threshold: (0..fantasy_points_threshold_edge)
-          }
-          Contest.where(filter_by_int.merge(filter_params))
+          Contest.all
         end
       end
     end
