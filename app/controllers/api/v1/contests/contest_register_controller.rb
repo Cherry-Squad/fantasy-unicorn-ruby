c427f8e2e4ad0f966fd04da6394dfa6d27964ec7
Габриэль Осипов <gabriel.osipov@saltpepper.ru>
2022-01-07 20:22:35 +0300

    use AssignStockPrice service for each contest_application_stock when user register in contest

--- app/controllers/api/v1/contests/contest_register_controller.rb
+++ app/controllers/api/v1/contests/contest_register_controller.rb
@@ -12,11 +12,11 @@ module Api
           if contest
             contest_application = create_contest_application contest
             contest_app_stocks = array_of_contest_app_stocks contest_application
-            render json: { contest_app_stocks: contest_app_stocks }, status: 404
+            assign_stocks_prices contest_app_stocks, contest_application.id
+            render json: { contest_app_stocks: contest_app_stocks }, status: 201
           else
             render json: { status: 'Not Found 404' }, status: 404
           end
-
         rescue StandardError => e
           render json: { error: "an error occurred #{e.message}" }, status: 400
         end
@@ -43,17 +43,23 @@ module Api
             contest_application_stock.save
             array.append contest_application_stock
           end
-          array
+          array.delete_if { |el| el[:id].nil? }
+        end
+
+        def assign_stocks_prices(contest_app_stocks, contest_app_id)
+          contest_app_stocks.each do |contest_app_stock|
+            ContestsServices::AssignStockPrice.delay(queue: 'contest_processing')
+                                              .call contest_app_id, contest_app_stock.stock_id, Time.now.utc, 'req'
+          end
         end
 
         def create_params
-          params.require(:contest_register).permit({ items: [:stock_id, :multiplier] })
+          params.require(:contest_register).permit({ items: %i[stock_id multiplier] })
         end
 
         def contest_by_id
           Contest.find_by(id: params[:id])
         end
-
       end
     end
   end
