55b6206c89d86b01dde64d51dff46208f642a978
Габриэль Осипов <gabriel.osipov@saltpepper.ru>
2022-01-09 21:47:53 +0300

    refactor code using rubocop code style

--- config/initializers/finance_api.rb
+++ config/initializers/finance_api.rb
@@ -4,9 +4,7 @@ require 'finnhub_ruby'
 
 Rails.application.configure do
   config.finnhub_config = (Rails.application.config_for :finance_api)['finnhub']
-  if config.finnhub_config[:enable]
-    FinnhubRuby.configure.api_key['api_key'] = config.finnhub_config[:api_key]
-  end
+  FinnhubRuby.configure.api_key['api_key'] = config.finnhub_config[:api_key] if config.finnhub_config[:enable]
 
   config.time_shift = config.finnhub_config[:time_shift]
 end
